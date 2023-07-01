990ab6cb49bb25e1f660c9a0e56228decde94fc5
melsaied <m.elsaied@gmail.com>
2023-06-30 21:53:24 -0500

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 	@GetMapping
 	public String serverUp() {
 		return "server up 0.1";
